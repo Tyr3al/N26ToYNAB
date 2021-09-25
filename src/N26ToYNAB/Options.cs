dd50dffef421f2cd965b0bec6f972cad248230e5
Jan Funke <jan.funke@semodia.com>
2021-09-25 14:43:53 +0200

    Added: Initial code

---
+++ src/N26ToYNAB/Options.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace N26ToYNAB
+{
+    /// <summary>
+    /// Available command line options
+    /// </summary>
+    public class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Path to N26 CSV file")]
+        public string InputCSV { get; set; }
+        
+        [Option('o', "output", Required = false, HelpText = "Path to generated YNAB csv (optional)")]
+        public string OutputCSV { get; set; }
+    }
+}
